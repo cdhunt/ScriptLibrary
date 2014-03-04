5dd14fa3139289c8c89451a2c37326b04840e345
Chris Hunt <cdhunt@gmail.com>
2014-03-04 15:15:33 -0500

    get mirrorpath per documentation

--- thetvdbapiconsumer.ps1
+++ thetvdbapiconsumer.ps1
@@ -1,12 +1,20 @@
 # http://thetvdb.com/wiki/index.php?title=Programmers_API
 $apikey = '123'
 
+# Separate the mirrors using the typemask field, as documented for mirrors.xml.
+# 1 xml files
+# 2 banner files
+# 4 zip files
+# Select a random mirror from each array (denoted as <mirrorpath_xml>, <mirrorpath_banners>, and <mirrorpath_zip> for rest of example.
 $mirrors = irm "http://thetvdb.com/api/$apikey/mirrors.xml"
-$mirrorpath = $mirrors.Mirrors.Mirror.mirrorpath
+$mirrorpath = $mirrors.Mirrors.Mirror | ? {$_.typemask -band 1} | Get-Random | select -ExpandProperty mirrorpath
 
+# Save this in your code and allow your users to select their language (denoted as <language> for rest of example). 
+# Note: You may also grab this dynamically when needed, but it'll rarely be changed.
 $languages = irm "$mirrorpath/api/$apikey/languages.xml"
 $english = $languages.Languages.Language | ? name -eq English
 
+# Store this value for later use (denoted as <previoustime> for rest of example)
 $currenttime = irm "http://thetvdb.com/api/Updates.php?type=none"
 
 $series = [ordered]@{
